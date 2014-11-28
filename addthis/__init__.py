b8d9f3223d10182f24754de63354dcbb7a924c6e
Alex Parinov <creafz@gmail.com>
2014-11-28 11:20:01 +0400

    Add imports and package info to __init__.py

---
+++ addthis/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from addthis.api import Addthis
+
+from addthis.exceptions import AddthisError, AddthisValidationError
+
+__author__ = 'Alex Parinov'
+__author_email__ = 'creafz@gmail.com'
+__license__ = 'MIT'
+__version__ = '0.1.0'
+
+__all__ = [
+    '__author__',
+    '__email__',
+    '__license__',
+    '__version__',
+    'AddthisError',
+    'AddthisValidationError',
+    'Addthis'
+]
